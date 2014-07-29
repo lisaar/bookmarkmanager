0195797445fe3355440907492544ee8f587c44d2
Lisa A-R <lisaar@gmail.com>
2014-07-29 11:30:02 +0100

    validates uniqueness of email

--- lib/user.rb
+++ lib/user.rb
@@ -5,7 +5,7 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, :unique => true, :message => "This email is already taken"
   property :password_digest, Text
 
   def password=(password)
@@ -14,9 +14,10 @@ class User
 
   attr_reader :password
   attr_accessor :password_confirmation
-  validates_confirmation_of :password 
+  validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
   def password=(password) 
      @password = password
     self.password_digest = BCrypt::Password.create(password)
   end 
+
 end
